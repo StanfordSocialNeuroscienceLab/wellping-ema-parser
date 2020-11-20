e31b452209633ad3d727b6f8c5dac56e32df453e
Ian <47256454+IanRFerguson@users.noreply.github.com>
2020-11-19 20:11:41 -0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -19,22 +19,21 @@ def cleanAnswers(DF):
     For answers data in long format
     """
 
-    DF['preferNotToAnswer'] = DF['preferNotToAnswer'].astype(str)
+    DF['preferNotToAnswer'] = DF['preferNotToAnswer'].astype(str)       # Convert PNA column to string
 
     for ix, val in enumerate(DF["data"]):
         try:
-            temp = val.values()
+            temp = val.values()                                         # Isolate participant responses, if not NA
             DF.loc[ix, "data"] = str(temp)
-
         except:
             DF.loc[ix, "data"] = val
 
         if DF['preferNotToAnswer'][ix] == "True":
-            DF.loc[ix, "data"] = "PNA"
+            DF.loc[ix, "data"] = "PNA"                                  # Fill PNA values appropriately
         else:
             continue
 
-    DF['data'] = DF['data'].map(lambda x: str(x)[13:-2])
+    DF['data'] = DF['data'].map(lambda x: str(x)[13:-2])                # Strip dictionary brackets
     return DF
 
 
@@ -42,14 +41,14 @@ def preferNotToAnswer(DF):
     """
     Replaces data values with "PNA" when applicable
     """
-    for ix, val in enumerate(DF['preferNotToAnswer']):
-        if str(val) != "None":
+    for ix, val in enumerate(DF['preferNotToAnswer']):                  # Another PNA iteration...
+        if str(val) != "None":                                          # In case the cleanAnswers() v doesn't work lol
             DF["data"][ix] = "PNA"
 
 
 def flattenAnswers(DF):
     """
-    For answers data in long format
+    Converts DF from long to wide format
     """
 
     DF["IX"] = DF.groupby("questionId", as_index=False).cumcount()
@@ -66,18 +65,17 @@ def splitStressResponses(DF):
         real_vals = []
 
         try:
-            clean = response.strip("[").strip("]").split("]")
+            clean = response.strip("[").strip("]").split("]")               # Strip outside brackets
         except:
             continue
 
         for item in clean:
             if "True" in item:
-                item = item.strip(",").strip("[").split(",")
-                real_vals.append(item[0].strip("["))
+                item = item.strip(",").strip("[").split(",")                # Strip inside brackets
+                real_vals.append(item[0].strip("["))                        # Add to empty list
 
         try:
-            DF.loc[idx, 'stressResponse'] = real_vals
-
+            DF.loc[idx, 'stressResponse'] = real_vals                       # Replace column w/ clean values
         except:
             DF.loc[idx, 'stressResponse'] = "NA"
 
@@ -87,18 +85,20 @@ def splitNominations(DF):
     If applicable, splits SU_Nom column into three distinct columns
     """
 
+    # Default to NA values
     DF["SU_Nom_1"] = "NA"
     DF["SU_Nom_2"] = "NA"
     DF["SU_Nom_3"] = "NA"
 
     for ix, response in enumerate(DF.loc[:, "SU_Nom"]):
         try:
-            temp = response.split(",")
+            temp = response.split(",")                                      # Split nominations into list on ','
         except:
             temp = response
 
+        # Not efficient but it works...
         try:
-            DF.loc[ix, "SU_Nom_1"] = temp[0].strip("[").title()
+            DF.loc[ix, "SU_Nom_1"] = temp[0].strip("[").title()             # Push clean responses to correct columns
         except:
             continue
 
@@ -118,10 +118,12 @@ def splitNSU(DF):
     If applicable, splits SU_Nom column into three distinct columns
     """
 
+    # Default to NA
     DF["NSU_Rel_1"] = "NA"
     DF["NSU_Rel_2"] = "NA"
     DF["NSU_Rel_3"] = "NA"
 
+    # See .splitNominations() ... same philosophy here
     for ix, response in enumerate(DF.loc[:, "NSU_Rel"]):
         try:
             temp = response.split(",")
@@ -175,7 +177,6 @@ def defineInteractions(DF, LOG):
 
             try:
                 DF.loc[idx, var] = real_vals
-
             except:
                 DF.loc[idx, var] = "NA"
 
@@ -187,26 +188,29 @@ def splitRace(DF):
     """
 
     for idx, response in enumerate(DF['Race']):
-        real_vals = []
+        real_vals = []                                                          # Empty list to append into
 
         try:
-            clean = response.strip("[").strip("]").split("]")
+            clean = response.strip("[").strip("]").split("]")                   # Strip outside brackets
         except:
             continue
 
         for item in clean:
             if "True" in item:
-                item = item.strip(",").strip("[").split(",")
-                real_vals.append(item[0].strip("["))
+                item = item.strip(",").strip("[").split(",")                    # Strip inside brackets
+                real_vals.append(item[0].strip("["))                            # Add to list
 
         try:
-            DF.loc[idx, 'Race'] = real_vals
-
+            DF.loc[idx, 'Race'] = real_vals                                     # Replace Race column with True vals
         except:
             DF.loc[idx, 'Race'] = "NA"
 
 
 def cleanup(DF):
+    """
+    Strips out extraneous characters for every item in each column
+    """
+
     for var in DF.columns:
         for char in ['[',']','\'','"']:
             DF[var] = DF[var].map(lambda x: str(x).replace(char, ""))
@@ -226,11 +230,15 @@ def addUsername(DF, IX):
 
 # ------------------ WRAPPER
 def setup(here):
+    """
+    Check to see if output directories exist
+    If they don't - create them!
+    """
+
     for repo in ["89_Participant-Files", "99_Composite-CSV"]:
         try:
             os.chdir(repo)
             os.chdir(here)
-
         except:
             print("Creating {} directory...".format(repo))
             os.mkdir(repo)
@@ -239,25 +247,33 @@ def setup(here):
 
 
 def grabJSON(DIR):
+    """
+    Finds JSON file in the target directory and isolates it
+    """
+
     for file in os.listdir(DIR):
         if file.endswith(".json"):
             return file
-
         else:
             continue
 
 
 def output(filename="Composite-Responses"):
-    os.chdir("89_Participant-Files")
-    output = pd.DataFrame()
+    """
+    Loop through participant-wise CSVs
+    Append to output DF and save it to landing directory
+    """
+
+    os.chdir("89_Participant-Files")                                            # Target directory
+    output = pd.DataFrame()                                                     # Empty DF to append into
 
     for file in tqdm(os.listdir()):
+        # Append to output DF
         output = output.append(pd.read_csv(file), ignore_index=True, sort=False)
 
-    output.fillna("NA", inplace=True)
-    os.chdir("..")
-    os.chdir("99_Composite-CSV")
-    output.to_csv("{}.csv".format(filename), index=False)
+    output.fillna("NA", inplace=True)                                           # Fill NA values with 'NA'
+    os.chdir("../99_Composite-CSV")                                             # Move to landing directory
+    output.to_csv("{}.csv".format(filename), index=False)                       # Push to CSV
     os.chdir("../..")
 
 
@@ -271,11 +287,14 @@ def parseReponses(DATA, IX, LOG):
     LOG <- Text file to keep track of errors
     """
 
-    filename = "{}.csv".format(IX)
-    output_path = "89_Participant-Files/"
-    big_kahuna = pd.DataFrame()
+    filename = "{}.csv".format(IX)                      # Format filename to be exported
+    output_path = "89_Participant-Files/"               # Landing directory for output CSVs
+    big_kahuna = pd.DataFrame()                         # Empty DF to append into
 
     for index in range(len(DATA.keys())):
+
+        # Isolate pings, answers, and devices sub-branches from participant JSON branches
+
         # ------- Pings
         pings = pd.DataFrame(DATA[list(DATA.keys())[index]]["pings"])
         addUsername(pings, IX)
@@ -289,10 +308,17 @@ def parseReponses(DATA, IX, LOG):
 
         # ------- Devices
         devices = pd.DataFrame(DATA[list(DATA.keys())[index]]['user']['installation']['device'], index=[0])
+        app = pd.DataFrame(DATA[list(DATA.keys())[index]]['user']['installation']['app'], index=[0])
+        user_total = devices.join(app)
+        device_columns = user_total.columns
 
         # ------ Kick-out
         output_combo = pings.join(flat_answers, on="id")
-        output_combo = output_combo.join(devices, how="left")
+        output_combo = output_combo.join(user_total, how="left")
+
+        for var in device_columns:
+            output_combo[var] = user_total[var][0]      # Fill length of DF columns with first device value
+
         big_kahuna = big_kahuna.append(output_combo, ignore_index=True, sort=False)
 
     try:
