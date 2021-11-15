f4382f8cae0dc25a3341967a440e3c213093ffd6
Ian <47256454+IanRFerguson@users.noreply.github.com>
2021-11-15 08:22:37 -0800

    Update devices.py

--- devices.py
+++ devices.py
@@ -17,16 +17,16 @@ def parse_device_info(SUBSET):
     
     """
 
-    devices = SUBSET['user']
-    username = devices['username']
+    devices = SUBSET['user']                                            #
+    username = devices['username']                                      #
 
-    master = pd.DataFrame({'username':username}, index=[0])
+    master = pd.DataFrame({'username':username}, index=[0])             #
 
     for key in ['device', 'app']:
-        temp = devices['installation'][key]
-        temp_frame = pd.DataFrame(temp, index=[0])
-        temp_frame['username'] = username
+        temp = devices['installation'][key]                             #
+        temp_frame = pd.DataFrame(temp, index=[0])                      #
+        temp_frame['username'] = username                               #
         
-        master = master.merge(temp_frame, on='username')
+        master = master.merge(temp_frame, on='username')                #
 
     return master
