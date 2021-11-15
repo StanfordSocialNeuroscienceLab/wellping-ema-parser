215ae37cdc549759090115bbb6b00e35490aae3b
Ian <47256454+IanRFerguson@users.noreply.github.com>
2021-11-15 08:22:31 -0800

    Update ripper.py

--- ripper.py
+++ ripper.py
@@ -15,42 +15,58 @@ from devices import parse_device_info
 
 # ----- Run Script
 def main():
-      target_path = sys.argv[1]
-      setup(target_path)
-      sub_data, output_filename = isolate_json_file(target_path)
+      target_path = sys.argv[1]                                               #
+      setup(target_path)                                                      #
+      sub_data, output_filename = isolate_json_file(target_path)              #
 
+      #
       subject_output_directory = os.path.join(".", target_path, "00-Subjects")
       aggregate_output_directory = os.path.join(".", target_path, "01-Aggregate")
 
+      #
       with open(sub_data) as incoming:
-            with open(f"./{target_path}/{output_filename}.txt", "w") as log:
-                  data = json.load(incoming)
 
-                  keepers = []
-                  parent_errors = {}
+            #
+            with open(f"./{target_path}/{output_filename}.txt", "w") as log:
+                  
+                  data = json.load(incoming)                                  #
+
+                  keepers = []                                                #
+                  parent_errors = {}                                          #
 
                   print("\nParsing participant data...\n")
                   sleep(1)
 
+                  #
                   for key in tqdm(list(data.keys())):
-                        subset = data[key]
 
+                        subset = data[key]                                    #
+
+                        #
                         if len(subset['answers']) == 0:
                               parent_errors[key] = subset
                               continue
                         
                         try:
-                              parsed_data = parse_responses(key, subset, log, subject_output_directory)
+
+                              #
+                              parsed_data = parse_responses(key, subset, log, 
+                                                            subject_output_directory, True)
+
                         except Exception as e:
+
+                              #
                               log.write(f"\nCaught @ {key.split('-')[0]}: {e}\n\n")
                               continue
 
-                        keepers.append(parsed_data)
+                        keepers.append(parsed_data)                           #
 
                   sleep(1)
                   print("\nAggregating participant data...\n")
 
                   try:
+
+                        #
                         aggregate = pd.concat(keepers)
                         aggregate.to_csv(f'./{target_path}/01-Aggregate/pings_{output_filename}.csv',
                                           index=False, encoding="utf-8-sig")
@@ -59,24 +75,34 @@ def main():
 
                   print("\nSaving parent errors...\n")
 
+                  #
                   with open(f'./{target_path}/01-Aggregate/parent-errors.json', 'w') as outgoing:
                         json.dump(parent_errors, outgoing, indent=4)
 
                   print("\nParsing device information...\n")
 
+                  #
                   with open(f"./{target_path}/device-error-log.txt", 'w') as log:
 
-                        device_output = []
+                        device_output = []                                    #
 
+                        #
                         for key in tqdm(list(data.keys())):
-                              username = key.split('-')[0]
+
+                              username = key.split('-')[0]                    #
 
                               try:
+
+                                    #
                                     subset = data[key]
                                     device_output.append(parse_device_info(subset))
+
                               except Exception as e:
+
+                                    #
                                     log.write(f"\nCaught {username} @ device parser: {e}\n\n")
 
+                        #
                         devices = pd.concat(device_output)
                         devices.to_csv(f'./{target_path}/01-Aggregate/devices_{output_filename}.csv',
                                       index=False, encoding="utf-8-sig")
